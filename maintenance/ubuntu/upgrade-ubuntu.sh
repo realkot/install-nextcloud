7c0c82440a89170902e91ac246d0b0bbdb1d5e74
Carsten Rieger <47498887+criegerde@users.noreply.github.com>
2019-07-08 08:12:20 +0200

    removed deprecated statements

--- maintenance/ubuntu/upgrade-ubuntu.sh
+++ maintenance/ubuntu/upgrade-ubuntu.sh
@@ -1,4 +1,4 @@
-# Ubuntu 18.04.1
+# Ubuntu 18.04.2
 #!/bin/bash
 /usr/sbin/service nginx stop
 sudo -u www-data php /var/www/nextcloud/updater/updater.phar
@@ -6,8 +6,6 @@ sudo -u www-data php /var/www/nextcloud/occ status
 sudo -u www-data php /var/www/nextcloud/occ -V
 sudo -u www-data php /var/www/nextcloud/occ db:add-missing-indices
 sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint
-sudo -u www-data sed -i "s/upload_max_filesize=.*/upload_max_filesize=10240M/" /var/www/nextcloud/.user.ini
-sudo -u www-data sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
 sudo -u www-data sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
 sudo -u www-data php /var/www/nextcloud/occ app:update --all
 /usr/sbin/service php7.3-fpm restart
