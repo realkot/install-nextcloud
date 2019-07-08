62bd92ad3c6059a0a012d262ce59089cbf31e305
Carsten Rieger <47498887+criegerde@users.noreply.github.com>
2019-07-08 08:11:36 +0200

    removed deprecated statements

--- maintenance/debian/upgrade-debian.sh
+++ maintenance/debian/upgrade-debian.sh
@@ -6,8 +6,6 @@ su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ status'
 su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ -V'
 su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ db:add-missing-indices'
 su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ db:convert-filecache-bigint'
-sed -i "s/upload_max_filesize=.*/upload_max_filesize=10240M/" /var/www/nextcloud/.user.ini
-sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
 sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
 chown -R www-data:www-data /var/www/nextcloud
 su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ app:update --all'
